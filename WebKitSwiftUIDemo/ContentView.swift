be60c699a293f66d72eb29dc8cb0d900105caf10
RyosukeKamimura <come1234560@gmail.com>
2020-06-15 13:09:35 +0900

    finished

--- WebKitSwiftUIDemo/ContentView.swift
+++ WebKitSwiftUIDemo/ContentView.swift
@@ -7,15 +7,56 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    
+    @State private var urlString:String = "https://www.apple.com"
+    @State var isCheck:Bool = true
+    
+    
+    
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            HStack{
+                Button(action:{
+                    print("show url")
+                    self.isCheck.toggle()
+                    if self.isCheck == false{
+                        self.urlString = "https://www.google.com"
+                    }else{
+                        self.urlString = "https://www.youtube.com"
+                    }
+                }){
+                    Text("お気に入り")
+                }
+                Text(urlString)
+            }
+            WebView(url: URL(string: urlString)!)
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WebView: UIViewRepresentable{
+    var url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
     }
 }
